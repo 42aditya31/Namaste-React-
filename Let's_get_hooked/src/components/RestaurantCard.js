160201f0dc1e38826a4f5e489f41035638aab4e2
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-05-07 18:33:18 +0530

    fetching the data from the swiggy api key

--- Let's_get_hooked/src/components/RestaurantCard.js
+++ Let's_get_hooked/src/components/RestaurantCard.js
@@ -1,23 +1,19 @@
 import { MdStarRate } from "react-icons/md";
-import { CDN_URL } from "../utils/constants";
+import { IMG_CDN_URL } from "../../../../../../../../public/common/constants";
 
-const RestaurantCard = (props) => {
-  const { restaurantData } = props;
-
-  const {
-    cloudinaryImageId,
-    name,
-    areaName,
-    avgRating,
-    cuisines,
-    costForTwo,
-    deliveryTime,
-  } = restaurantData?.info;
-
+const RestaurantCard = ({
+  cloudinaryImageId,
+  name,
+  areaName,
+  sla,
+  cuisines,
+  costForTwo,
+  avgRating,
+}) => {
   return (
     <div className="restaurant-card">
       <img
-        src={CDN_URL + cloudinaryImageId}
+        src={IMG_CDN_URL + cloudinaryImageId}
         alt={name}
         className="restaurant-logo"
       />
@@ -28,11 +24,18 @@ const RestaurantCard = (props) => {
         </h3>
         <div className="esa-rating">
           <h4 className="rating">
-            <MdStarRate className="rating-logo" />
+            <MdStarRate
+              className="rating-logo"
+              style={
+                avgRating > 4.2
+                  ? { backgroundColor: "var(--green)" }
+                  : { backgroundColor: "var(--red)" }
+              }
+            />
             <span>{avgRating}</span>
           </h4>
           <h4>{costForTwo}</h4>
-          <h4>{deliveryTime} mins</h4>
+          <h4>{sla.deliveryTime} mins</h4>
         </div>
         <p className="cousine">
           {cuisines.join(", ").slice(0, 30)}
